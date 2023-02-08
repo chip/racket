be0ade5a993c5c8b1265710764a4cbfaac51955d
Chip Castle <chip@chipcastle.com>
2023-02-07 20:02:24 -0600

    Add passing test suite

--- exercises/practice/beer-song/beer-song-test.rkt
+++ exercises/practice/beer-song/beer-song-test.rkt
@@ -13,42 +13,26 @@
 
      (test-equal? "verse 8"
                   (verse 8)
-                  (string "8 bottles of beer on the wall, 8 bottles of beer.\n"
-                          "Take one down and pass it around, 7 bottles of beer on the wall.\n"))
+                  "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n")
 
      (test-equal? "verse 2"
                   (verse 2)
-                  (string "2 bottles of beer on the wall, 2 bottles of beer.\n"
-                          "Take one down and pass it around, 1 bottle of beer on the wall.\n"))
+                  "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n")
 
      (test-equal? "verse 1"
                   (verse 1)
-                  (string "1 bottle of beer on the wall, 1 bottle of beer.\n"
-                          "Take it down and pass it around, no more bottles of beer on the wall.\n"))
+                  "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n")
 
      (test-equal? "verse 0"
                   (verse 0)
-                  (string "No more bottles of beer on the wall, no more bottles of beer.\n"
-                          "Go to the store and buy some more, 99 bottles of beer on the wall.\n"))
+                  "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n")
 
      (test-equal? "song 8-6"
                   (song 8 6)
-                  (string "8 bottles of beer on the wall, 8 bottles of beer.\n"
-                          "Take one down and pass it around, 7 bottles of beer on the wall.\n\n"
-                          "7 bottles of beer on the wall, 7 bottles of beer.\n"
-                          "Take one down and pass it around, 6 bottles of beer on the wall.\n\n"
-                          "6 bottles of beer on the wall, 6 bottles of beer.\n"
-                          "Take one down and pass it around, 5 bottles of beer on the wall.\n"))
+                  "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n\n7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 6 bottles of beer on the wall.\n\n6 bottles of beer on the wall, 6 bottles of beer.\nTake one down and pass it around, 5 bottles of beer on the wall.\n")
 
      (test-equal? "song 3-0"
                   (song 3 0)
-                  (string "3 bottles of beer on the wall, 3 bottles of beer.\n"
-                          "Take one down and pass it around, 2 bottles of beer on the wall.\n\n"
-                          "2 bottles of beer on the wall, 2 bottles of beer.\n"
-                          "Take one down and pass it around, 1 bottle of beer on the wall.\n\n"
-                          "1 bottle of beer on the wall, 1 bottle of beer.\n"
-                          "Take it down and pass it around, no more bottles of beer on the wall.\n\n"
-                          "No more bottles of beer on the wall, no more bottles of beer.\n"
-                          "Go to the store and buy some more, 99 bottles of beer on the wall.\n"))))
+                  "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n\n2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n\n1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n")))
 
   (run-tests suite))
--- exercises/practice/beer-song/beer-song.rkt
+++ exercises/practice/beer-song/beer-song.rkt
@@ -2,8 +2,42 @@
 
 (provide song verse)
 
-(define (song start [end 0])
-  (error "Not implemented yet"))
+(define last-verse "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n")
+
+(define (no-more n)
+  (if (= n 0) "no more" n))
+
+(define (singular-plural n)
+  (if (= n 1) "bottle" "bottles"))
+
+(define (bottles n)
+  (format "~a ~a" (no-more n) (singular-plural n)))
+
+(define of-beer "of beer")
+
+(define of-beer-on-the-wall (format "~a on the wall" of-beer))
+
+(define (verse-part-1 n)
+  (let ([s (bottles n)])
+    (format "~a ~a, ~a ~a" s of-beer-on-the-wall s of-beer)))
+
+(define (it-or-one n)
+  (if (= n 1) "it" "one"))
+
+(define (take-1-down n)
+  (format "Take ~a down and pass it around," (it-or-one n)))
+
+(define (compose-verse n)
+  (format "~a.\n~a ~a ~a.\n"
+          (verse-part-1 n)
+          (take-1-down n)
+          (bottles (sub1 n)) 
+          of-beer-on-the-wall))
 
 (define (verse n)
-  (error "Not implemented yet"))
+  (if (= n 0)
+    last-verse
+    (compose-verse n)))
+
+(define (song start [end 0])
+  (string-join (map (lambda (n) (verse n)) (inclusive-range start end -1)) "\n"))
