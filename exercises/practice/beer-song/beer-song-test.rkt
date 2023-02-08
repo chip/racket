0503938584da8a867b9e423721dc93ea725f6fe9
Chip Castle <chip@chipcastle.com>
2023-02-07 20:10:20 -0600

    Update test strings to use string-append for readabilty

--- exercises/practice/beer-song/beer-song-test.rkt
+++ exercises/practice/beer-song/beer-song-test.rkt
@@ -13,26 +13,48 @@
 
      (test-equal? "verse 8"
                   (verse 8)
-                  "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n")
+                  (string-append
+                    "8 bottles of beer on the wall, 8 bottles of beer.\n"
+                    "Take one down and pass it around, 7 bottles of beer on the wall.\n"))
 
      (test-equal? "verse 2"
                   (verse 2)
-                  "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n")
+                  (string-append
+                    "2 bottles of beer on the wall, 2 bottles of beer.\n"
+                    "Take one down and pass it around, 1 bottle of beer on the wall.\n"))
 
      (test-equal? "verse 1"
                   (verse 1)
-                  "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n")
+                  (string-append
+                    "1 bottle of beer on the wall, 1 bottle of beer.\n"
+                    "Take it down and pass it around, no more bottles of beer on the wall.\n"))
 
      (test-equal? "verse 0"
                   (verse 0)
-                  "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n")
+                  (string-append
+                    "No more bottles of beer on the wall, no more bottles of beer.\n"
+                    "Go to the store and buy some more, 99 bottles of beer on the wall.\n"))
 
      (test-equal? "song 8-6"
                   (song 8 6)
-                  "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n\n7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 6 bottles of beer on the wall.\n\n6 bottles of beer on the wall, 6 bottles of beer.\nTake one down and pass it around, 5 bottles of beer on the wall.\n")
+                  (string-append
+                    "8 bottles of beer on the wall, 8 bottles of beer.\n"
+                    "Take one down and pass it around, 7 bottles of beer on the wall.\n\n"
+                    "7 bottles of beer on the wall, 7 bottles of beer.\n"
+                    "Take one down and pass it around, 6 bottles of beer on the wall.\n\n"
+                    "6 bottles of beer on the wall, 6 bottles of beer.\n"
+                    "Take one down and pass it around, 5 bottles of beer on the wall.\n"))
 
      (test-equal? "song 3-0"
                   (song 3 0)
-                  "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n\n2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n\n1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n")))
+                  (string-append
+                    "3 bottles of beer on the wall, 3 bottles of beer.\n"
+                    "Take one down and pass it around, 2 bottles of beer on the wall.\n\n"
+                    "2 bottles of beer on the wall, 2 bottles of beer.\n"
+                    "Take one down and pass it around, 1 bottle of beer on the wall.\n\n"
+                    "1 bottle of beer on the wall, 1 bottle of beer.\n"
+                    "Take it down and pass it around, no more bottles of beer on the wall.\n\n"
+                    "No more bottles of beer on the wall, no more bottles of beer.\n"
+                    "Go to the store and buy some more, 99 bottles of beer on the wall.\n"))))
 
   (run-tests suite))
